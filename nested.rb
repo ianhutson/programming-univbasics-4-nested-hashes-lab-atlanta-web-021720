1ce118deac04ee74cce15c8735a2f1e862684f50
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-09 19:19:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,6 +78,8 @@ def adding_matz
      }
  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+ 
+ puts programmer_hash
 
 	programmer_hash = 
  		{
