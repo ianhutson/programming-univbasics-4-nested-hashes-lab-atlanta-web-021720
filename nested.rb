fe1b1fd0a839bdeccd9fe7bb4592b6de9434cecb
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-09 19:23:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,24 +81,7 @@ def adding_matz
  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
  programmer_hash
 end
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => "C"
-     }
- 		}
 
-    
-end
 
 def changing_alan
 	programmer_hash = 
